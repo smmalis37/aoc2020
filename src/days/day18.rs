7a838a23503f43a1e8c3974a82af958d8e665387
Steven Malis <smmalis37@gmail.com>
2020-12-17 23:29:39 -0800

    Make day 18 better.

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,129 +1,124 @@
-#![allow(clippy::cast_lossless)]
 use crate::day_solver::DaySolver;
 
 pub struct Day18;
 
-#[derive(Copy, Clone)]
-pub enum Token {
-    Num(u8),
-    Add,
-    Mult,
-    OpenParen,
-    CloseParen,
-}
-
-use Token::*;
-
 type N = u64;
 
-impl DaySolver<'_> for Day18 {
-    type Parsed = Vec<Vec<Token>>;
+#[derive(Clone, Copy)]
+enum Op {
+    Add,
+    Mult,
+}
+
+use Op::*;
+
+fn real_parse(x: &[u8]) -> impl Iterator<Item = impl Iterator<Item = u8> + '_> {
+    x.split(|&x| x == b'\n')
+        .map(|l| l.iter().filter(|&&x| x != b' ').copied())
+}
+
+impl<'a> DaySolver<'a> for Day18 {
+    type Parsed = &'a [u8];
     type Output = N;
 
-    fn parse(input: &str) -> Self::Parsed {
-        input
-            .as_bytes()
-            .split(|&x| x == b'\n')
-            .map(|l| {
-                l.iter()
-                    .filter_map(|c| match c {
-                        b'0'..=b'9' => Some(Num(c - b'0')),
-                        b'+' => Some(Add),
-                        b'*' => Some(Mult),
-                        b'(' => Some(OpenParen),
-                        b')' => Some(CloseParen),
-                        b' ' => None,
-                        _ => unreachable!(),
-                    })
-                    .collect()
-            })
-            .collect()
+    fn parse(input: &'a str) -> Self::Parsed {
+        input.as_bytes()
     }
 
     fn part1(data: Self::Parsed) -> Self::Output {
-        data.into_iter().map(|e| eval(&e).0).sum()
+        real_parse(data).map(|mut e| eval(&mut e)).sum()
     }
 
     fn part2(data: Self::Parsed) -> Self::Output {
-        data.into_iter().map(|e| eval(&transform(e)).0).sum()
+        real_parse(data).map(|e| eval(&mut transform(e))).sum()
     }
 }
 
-fn transform(mut e: Vec<Token>) -> Vec<Token> {
-    e.insert(0, OpenParen);
-
-    let mut i = 1;
-    while i < e.len() {
-        match e[i] {
-            Mult => {
-                e.insert(i, CloseParen);
-                e.insert(i + 2, OpenParen);
-                i += 2;
-            }
-            OpenParen => {
-                e.insert(i, OpenParen);
-                i += 1;
-            }
-            CloseParen => {
-                e.insert(i, CloseParen);
-                i += 1;
-            }
-            _ => {}
-        }
-        i += 1;
+fn transform(source: impl Iterator<Item = u8>) -> impl Iterator<Item = u8> {
+    AdditionWrapper {
+        source,
+        previous: Some(b'('),
+        previous2: None,
     }
-
-    e.push(CloseParen);
-
-    e
 }
 
-fn eval(tokens: &[Token]) -> (N, usize) {
-    let mut prev_op = None;
+struct AdditionWrapper<T: Iterator<Item = u8>> {
+    source: T,
+    previous: Option<u8>,
+    previous2: Option<u8>,
+}
 
-    let (mut result, position) = match tokens[0] {
-        Num(x) => (x as N, 1),
-        OpenParen => {
-            let (r, mut p) = eval(&tokens[1..]);
-            p += 2;
-            (r, p)
+impl<T: Iterator<Item = u8>> Iterator for AdditionWrapper<T> {
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(x) = self.previous {
+            self.previous = self.previous2.take();
+            Some(x)
+        } else if let Some(x) = self.source.next() {
+            match x {
+                b'*' => {
+                    self.previous = Some(x);
+                    self.previous2 = Some(b'(');
+                    Some(b')')
+                }
+                b'(' => {
+                    self.previous = Some(b'(');
+                    Some(b'(')
+                }
+                b')' => {
+                    self.previous = Some(b')');
+                    Some(b')')
+                }
+                b' ' => unreachable!(),
+                x => Some(x),
+            }
+        } else {
+            Some(b')')
         }
-        _ => unreachable!(),
-    };
+    }
+}
 
-    let mut position = position..tokens.len();
-    while let Some(i) = position.next() {
-        match tokens[i] {
-            Num(x) => {
-                result = apply_op(prev_op, result, x as N);
+fn eval(tokens: &mut impl Iterator<Item = u8>) -> N {
+    let mut prev_op = Some(Add);
+    let mut result = 0;
+
+    while let Some(i) = tokens.next() {
+        match i {
+            x @ b'0'..=b'9' => {
+                result = apply_op(prev_op.unwrap(), result, convert(x));
                 prev_op = None;
             }
-            Add => {
+            b'+' => {
                 debug_assert!(prev_op.is_none());
                 prev_op = Some(Add);
             }
-            Mult => {
+            b'*' => {
                 debug_assert!(prev_op.is_none());
                 prev_op = Some(Mult);
             }
-            OpenParen => {
-                let (res, pos) = eval(&tokens[i + 1..]);
-                result = apply_op(prev_op, result, res);
-                position.nth(pos);
+            b'(' => {
+                let res = eval(tokens);
+                result = apply_op(prev_op.unwrap(), result, res);
                 prev_op = None;
             }
-            CloseParen => return (result, i),
+            b')' => break,
+            _ => unreachable!(),
         }
     }
 
-    (result, tokens.len())
+    result
 }
 
-fn apply_op(prev_op: Option<Token>, result: N, x: N) -> N {
+#[allow(clippy::cast_lossless)]
+fn convert(x: u8) -> N {
+    (x - b'0') as N
+}
+
+fn apply_op(prev_op: Op, result: N, x: N) -> N {
     match prev_op {
-        Some(Add) => result + x,
-        Some(Mult) => result * x,
-        _ => unreachable!(),
+        Add => result + x,
+        Mult => result * x,
     }
 }
 
